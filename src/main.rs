2916215122ac0cd5d7079ad223d7e93b8bcac498
Jeff Shen <jshen2014@hotmail.com>
2020-07-13 00:08:31 -0400

    clippy and rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
-use std::env;
-use std::fs::{File, read_to_string};
-use std::io::{Write, BufReader, BufRead};
-use std::path::Path;
-use std::ffi::OsStr;
-use walkdir::WalkDir;
+use clap::{crate_authors, crate_version, App, Arg, ArgMatches};
 use regex::Regex;
-use clap::{App, Arg, ArgMatches, crate_authors, crate_version};
+use std::env;
+use std::ffi::OsStr;
+use std::fs::{read_to_string, File};
+use std::io::{BufRead, BufReader, Write};
+use std::path::Path;
+use walkdir::WalkDir;
 
-fn cache(cachepath: &str, filters: &Vec<&str>) {
-
-    let file_filters = filters[0].trim().split(" ").into_iter().map(|f| OsStr::new(f)).collect::<Vec<_>>();
-    let path_filters = filters[1].trim().split(" ").into_iter().map(|f| Path::new(f)).collect::<Vec<_>>();
+fn cache(cachepath: &str, filters: &[&str]) {
+    let file_filters = filters[0]
+        .trim()
+        .split(' ')
+        .map(|f| OsStr::new(f))
+        .collect::<Vec<_>>();
+    let path_filters = filters[1]
+        .trim()
+        .split(' ')
+        .map(|f| Path::new(f))
+        .collect::<Vec<_>>();
 
     let mut output = File::create(cachepath).unwrap();
-    for entry in WalkDir::new("/").follow_links(true)
+    for entry in WalkDir::new("/")
+        .follow_links(true)
         .into_iter()
-        .filter_entry(|d| (&d.path() == &Path::new("/")) || (!file_filters.contains(&d.path().file_name().unwrap()) && !path_filters.contains(&d.path().parent().unwrap())))
-        .filter_map(|e| e.ok()) {
-        write!(output, "{}\n", entry.path().display()).unwrap();
+        .filter_entry(|d| {
+            (d.path() == Path::new("/"))
+                || (!file_filters.contains(&d.path().file_name().unwrap())
+                    && !path_filters.contains(&d.path().parent().unwrap()))
+        })
+        .filter_map(|e| e.ok())
+    {
+        writeln!(output, "{}", entry.path().display()).unwrap();
     }
 }
 
@@ -25,16 +38,16 @@ fn find(path: &str, pattern: &str) {
     let regexpattern = Regex::new(pattern).unwrap();
     let input = File::open(path).unwrap();
     let buffered = BufReader::new(input);
-    buffered.lines()
+    buffered
+        .lines()
         .map(|line| line.unwrap())
         .filter(|line| regexpattern.is_match(line))
         .for_each(|m| println!("{}", m));
 }
 
 fn main() {
-
     let cfg_file = read_to_string("/etc/locate-rs.conf").unwrap();
-    
+
     let options: ArgMatches = App::new("locate clone built with Rust")
         .author(crate_authors!())
         .version(crate_version!())
@@ -49,13 +62,12 @@ fn main() {
             .conflicts_with("cache"))
         .get_matches();
 
-
     if let Some(pattern) = options.value_of("search") {
         find("/tmp/locate-rs.cache", pattern);
     }
 
     if options.is_present("cache") {
-        let filters = cfg_file.trim().split("\n").collect::<Vec<_>>();
+        let filters = cfg_file.trim().split('\n').collect::<Vec<_>>();
         cache("/tmp/locate-rs.cache", &filters);
-    } 
+    }
 }
